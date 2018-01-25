b6ddd32709c2b607946a5f1fc958601785f316c7
brickbite <jeffreylee0116@gmail.com>
2018-01-24 23:44:32 -0800

    seeding data with csv files

--- database/seed.js
+++ database/seed.js
@@ -1,14 +1,7 @@
 require('dotenv').config()
 const path = require('path');
-const dataDirectory = path.join(__dirname, './data')
+const dataDirectory = path.join(__dirname, './data/csv')
 console.log(dataDirectory);
-
-// const categoriesData = require('./data/categories.json');
-// const productsData = require('./data/products.json');
-// const customersData = require('./data/customers.json');
-// const ordersData = require('./data/orders.json');
-// const orders_productsData = require('./data/orders_products.json');
-// const products_categoriesData = require('./data/products_categories.json');
 
 const { Client } = require('pg');
 const client = new Client({
@@ -18,9 +11,42 @@ const client = new Client({
   password: null
 });
 
-client.connect()
+client.connect();
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  client.end()
-})
+let tablesSeeded = 0;
+
+client.query(`COPY categories FROM '${dataDirectory}/categories.csv' DELIMITER ',' CSV;`, (err, res) => {
+  console.log(err ? err.stack : res);
+  tablesSeeded++;
+  tablesSeeded === 6 ? client.end() : null;
+});
+
+client.query(`COPY products FROM '${dataDirectory}/products.csv' DELIMITER ',' CSV;`, (err, res) => {
+  console.log(err ? err.stack : res);
+  tablesSeeded++;
+  tablesSeeded === 6 ? client.end() : null;
+});
+
+client.query(`COPY customers FROM '${dataDirectory}/customers.csv' DELIMITER ',' CSV;`, (err, res) => {
+  console.log(err ? err.stack : res);
+  tablesSeeded++;
+  tablesSeeded === 6 ? client.end() : null;
+});
+
+client.query(`COPY orders FROM '${dataDirectory}/orders.csv' DELIMITER ',' CSV;`, (err, res) => {
+  console.log(err ? err.stack : res);
+  tablesSeeded++;
+  tablesSeeded === 6 ? client.end() : null;
+});
+
+client.query(`COPY products_categories FROM '${dataDirectory}/products_categories.csv' DELIMITER ',' CSV;`, (err, res) => {
+  console.log(err ? err.stack : res);
+  tablesSeeded++;
+  tablesSeeded === 6 ? client.end() : null;
+});
+
+client.query(`COPY orders_products FROM '${dataDirectory}/orders_products.csv' DELIMITER ',' CSV;`, (err, res) => {
+  console.log(err ? err.stack : res);
+  tablesSeeded++;
+  tablesSeeded === 6 ? client.end() : null;
+});
