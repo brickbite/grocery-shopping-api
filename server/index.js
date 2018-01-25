095c154135b667f45818d96b39f4b862fd18e1b4
brickbite <jeffreylee0116@gmail.com>
2018-01-25 02:26:47 -0800

    added logger for requests

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const port = process.env | 3000;
 const routes = require('./routes');
 
 
+app.use((req, res, next) => {
+  console.log(`===== REQUEST: ${req.method} to ${req.url}. req.query: ${JSON.stringify(req.query)}`);
+  next();
+});
 
 app.use('/api/v0', routes);
 
