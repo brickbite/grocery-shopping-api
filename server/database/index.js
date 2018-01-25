b30a919aca4e87854a356264a323b290ea2f9c95
brickbite <jeffreylee0116@gmail.com>
2018-01-25 00:43:05 -0800

    db function for category query

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const { Client } = require('pg');
 const client = new Client({
   user: process.env.PGUSER,
@@ -8,9 +6,37 @@ const client = new Client({
   password: null
 });
 
-client.connect()
+client.connect();
 
 client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  client.end()
-})
+  client.end();
+});
+
+module.exports = {
+  categories: {
+    getAll: (cb) => {
+      client.query(`SELECT * FROM categories`, (err, res) => {
+        console.log(err ? err.stack : res);
+        cb(res);
+        client.end();
+      });
+    },
+    getName: (cb, name) => {
+      client.query(`SELECT * FROM categories WHERE category_name=${name}`, (err, res) => {
+        console.log(err ? err.stack : res);
+        cb(res);
+        client.end();
+      });
+    }
+  },
+  products: {
+
+  },
+  customers: {
+
+  },
+  orders: {
+
+  }
+};
