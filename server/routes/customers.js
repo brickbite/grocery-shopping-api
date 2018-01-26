7c689f3cb1f1b5334dffb8c9ba353c4aafeb2404
brickbite <jeffreylee0116@gmail.com>
2018-01-26 03:00:09 -0800

    added routes and logging for router

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -1,7 +1,14 @@
-const router = require('express').Router();
+const router = require('express').Router({mergeParams: true});
 const controllers = require('../controllers');
 
-router.get('/customers', controllers.customers.get)
+router.use((req, res, next) => {
+  console.log(`customers router: url: ${req.url} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)}`);
+  next();
+})
+
+router.get('/customers/orders', controllers.customers.getOrders)
+  .get('/customers/categories', controllers.customers.getCategories)
+  .get('/customers/*', controllers.customers.get)
   .post('/customers', controllers.customers.post);
 
 module.exports = router;
