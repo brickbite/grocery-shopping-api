5cc14515204a41fc4c175876087f35a5fbe57cb9
brickbite <jeffreylee0116@gmail.com>
2018-01-26 03:00:35 -0800

    modified routes, added logging for router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,13 @@
-const router = require('express').Router();
+const router = require('express').Router({mergeParams: true});
 const categoriesRouter = require('./categories');
 const productsRouter = require('./products');
 const customersRouter = require('./customers');
 const ordersRouter = require('./orders');
+
+router.use((req, res, next) => {
+  console.log(`index router: url: ${req.url} params: ${JSON.stringify(req.params)} query: ${JSON.stringify(req.query)}`);
+  next();
+})
 
 router.route('/categories')
   .get(categoriesRouter)
@@ -19,7 +24,7 @@ router.route('/products')
   .get(productsRouter)
   .post(productsRouter);
 
-router.route('/customers')
+router.route('/customers/:nextroute')
   .get(customersRouter)
   .post(customersRouter);
 
@@ -37,7 +42,7 @@ router.route('/customers')
 //   .get(ordersRouter)
 //   .post(ordersRouter);
 
-router.route('/orders')
+router.route('/orders/:nextroute')
   .get(ordersRouter)
   .post(ordersRouter);
 
