661a1f2e467e558f4fe66f09428d62bf634aa8b7
brickbite <jeffreylee0116@gmail.com>
2018-01-26 02:59:30 -0800

    functions for different routes

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -2,8 +2,19 @@ const db = require('../database');
 
 module.exports = {
   get: (req, res) => {
+    // dummy response for unmatched routes
     console.log('hello /customers get');
+    res.send('hello /customers get');
+  },
+  getOrders: (req, res) => {
+    console.log('hello /customers/orders get');
     db.customers.getCustomerOrders((err, queryResult) => {
+      err ? res.send(err) : res.send(queryResult);
+    }, req.query.customer_id);
+  },
+  getCategories: (req, res) => {
+    console.log('hello /customers/categories get');
+    db.customers.getCustomerOrdersByCategory((err, queryResult) => {
       err ? res.send(err) : res.send(queryResult);
     }, req.query.customer_id);
   },
